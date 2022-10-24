9ae1be253047c3d8659f45a4fb6f7511f4bc5995
wuxiaowei <252986979@qq.com>
2022-10-24 14:34:38 +0800

    Revert "first"
    This reverts commit 928d80d238b234fa9067ab1ae5bed4dc46d5fb99.

--- example/android/app/src/main/kotlin/com/cn/aws/broadcast/aws_broadcast_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cn.aws.broadcast.aws_broadcast_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- ios/Classes/AwsBroadcastPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface AwsBroadcastPlugin : NSObject<FlutterPlugin>
-@end
